daeb3be37db41582a4d74c148cafaa4e66457a5a
Juan Antonio Guzmán Hernández <jhonguzmanhernandez659@gmail.com>
2023-05-29 19:39:30 -0600

    Se agregaron dos imágenes en la vista 2

--- operaciones/vista2.php
+++ operaciones/vista2.php
@@ -2,11 +2,22 @@
 <html>
     <head>
         <title>Vista 2</title>
+        <style>
+            body{
+                background-color: grey;
+            }
+        </style>
 
     </head>
     <body>
         <div>
             <h5>Esta es la vista 2</h5>
+            <h2>imagenes</h2>
+
+            <div>
+                <img src="../images/drop.jpg">
+                <img src="../images/sinuser.jpg">
+            </div>
         </div>
 
     </body>
